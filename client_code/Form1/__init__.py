90e6737e5d857af7203726bc53b67d15f685cb21
infogautammanish@gmail.com <infogautammanish@gmail.com>
2023-09-27 21:12:53 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +11,14 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name=self.text_box_1.text
+    weight=int(self.text_box_3.text)
+    address=self.text_area_1.text
+    personal=self.check_box_1.checked
+    anvil.server.call('submit', name=name, address=address, weight=weight, personal=personal)
+    Notification("Your response has been recorded").show()
+    
+
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,23 @@
+import anvil.email
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def submit(name, weight, address, personal):
+  app_tables.gym.add_row(name=name, address=address, weight=weight, personal=personal)
+  anvil.email.send("kumargautam8666@gmail.com", subject="Your response from anvil app", text="Feedback from {name}: weight is {weight} and they live at: {address}. personal training: {personal}")
