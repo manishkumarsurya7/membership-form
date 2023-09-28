cf72bac198355e0f7895c6856d7a2cac93333e68
infogautammanish@gmail.com <infogautammanish@gmail.com>
2023-09-28 17:32:26 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 
 class Form1(Form1Template):
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,6 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.email
 import anvil.server
 
